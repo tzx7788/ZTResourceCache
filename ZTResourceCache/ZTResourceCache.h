33e07f9516e1f735b4826f63a1a0f276d1547c67
Tang Zhixiong <tzx7788@me.com>
2016-01-30 15:59:29 +0800

    initial commit

---
+++ ZTResourceCache/ZTResourceCache.h
@@ -0,0 +1,19 @@
+//
+//  ZTResourceCache.h
+//  ZTResourceCache
+//
+//  Created by Tang Zhixiong on 30/01/2016.
+//  Copyright © 2016 Tang Zhixiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZTResourceCache.
+FOUNDATION_EXPORT double ZTResourceCacheVersionNumber;
+
+//! Project version string for ZTResourceCache.
+FOUNDATION_EXPORT const unsigned char ZTResourceCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZTResourceCache/PublicHeader.h>
+
+
